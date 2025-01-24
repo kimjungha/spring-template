aae6071b3123fd4b173ecd602e704dbd68a52088
jungha.kim <jungha.kim@timf.co.kr>
2025-01-24 16:36:01 +0900

    템플릿 초기 상태1

---
+++ src/test/java/com/template/sprintTemplate/SprintTemplateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.template.sprintTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("스프링 템플릿");
+	}
+
+}

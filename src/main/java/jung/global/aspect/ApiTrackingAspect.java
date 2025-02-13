2d9b8d249add0f3083d86c577375e9ce13c9e105
jungha.kim <jungha.kim@timf.co.kr>
2025-02-13 17:41:33 +0900

    caching 성능 비교 위해 AOP 추가

--- src/main/java/jung/global/aspect/ApiTrackingAspect.java
+++ src/main/java/jung/global/aspect/ApiTrackingAspect.java
@@ -1,5 +1,6 @@
 package jung.global.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,26 +9,40 @@ import org.springframework.stereotype.Component;
 /** AOP */
 @Aspect
 @Component
+@Slf4j
 public class ApiTrackingAspect {
-//    @Around("annotation(*jung.global.annotation)") 애노테이션 적용
-//    @Around("execution(* jung.api..*Controller.*(..))") 컨트롤러에 적용
 
-    @Pointcut("@annotation(*jung.global.annotation.ApiTrackingCustom)")
+    // @Around("annotation(*jung.global.annotation)") 애노테이션 적용
+   // @Around("execution(* jung.api..*Controller.*(..))") 컨트롤러에 적용
+    @Pointcut("execution(* jung.api.erp.book.controller..*.*(..))")
     private void trackingController() { /* Point CUT이므로 로직 없음 */ }
-
     @Around("trackingController()")
-    public Object saveApiTrackingAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
-        boolean isSuccess = true;
-        Object result = null;
-        try{
-            result = joinPoint.proceed();
-        }catch(Throwable e){
-            isSuccess = false;
-        }
-        // 메인 타겟 실행 완료 후에, 사용자 로그 저장
-        saveLog(joinPoint,result,isSuccess);
-        return result;
+    public Object checkCachePerformance(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+
+        log.info("API 실행");
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+
+        return proceed;
     }
+
+
+//    @Around("trackingController()")
+//    public Object saveApiTrackingAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
+//        boolean isSuccess = true;
+//        Object result = null;
+//        try{
+//            result = joinPoint.proceed();
+//        }catch(Throwable e){
+//            isSuccess = false;
+//        }
+//        // 메인 타겟 실행 완료 후에, 사용자 로그 저장
+//        saveLog(joinPoint,result,isSuccess);
+//        return result;
+//    }
 
     private void saveLog(ProceedingJoinPoint joinPoint,Object result,boolean isSuccess){
 

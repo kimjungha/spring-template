797c9bdb0e9115d1d02e2642ad9769b3a07fc118
kimjungha <kimjungha0110@gmail.com>
2026-01-22 14:30:22 +0900

    인터페이스 메서드 public 제거 (컴파일러 자동으로 처리함)

--- src/main/java/jung/api/erp/coupon/service/CouponService.java
+++ src/main/java/jung/api/erp/coupon/service/CouponService.java
@@ -1,6 +1,6 @@
 package jung.api.erp.coupon.service;
 
 public interface CouponService{
-    public void couponDecrease(Long couponId);
-    public void couponDistributedDecrease(String lockName,Long couponId);
+    void couponDecrease(Long couponId);
+    void couponDistributedDecrease(String lockName,Long couponId);
 }

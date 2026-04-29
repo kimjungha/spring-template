107015a9529692732a2d97d051b524d449598aff
kimjungha <kimjungha0110@gmail.com>
2026-04-29 17:45:56 +0900

    book read api role, authority add

--- src/main/java/jung/api/erp/book/controller/BookReadController.java
+++ src/main/java/jung/api/erp/book/controller/BookReadController.java
@@ -4,17 +4,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import jung.api.erp.book.controller.response.BookReadResponse;
 import jung.api.erp.book.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/erp/read/book")
 public class BookReadController {
+
     private final BookService bookService;
+
+    @PreAuthorize("hasRole('USER') and hasAuthority('READ')")
     @GetMapping("/{bookId}")
     @Operation(summary = "책 조회", description = "책 상세 내용을 조회하는 API 입니다")
     public BookReadResponse findBook(@PathVariable Long bookId) {
